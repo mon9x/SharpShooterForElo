d9627a33ffc3d38ea14250b33797f961cb338a63
mon9x <h.mon9x@gmail.com>
2017-02-21 00:56:11 +0700

    Add files via upload

---
+++ SharpShooter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LeagueSharp.Common;
+
+namespace SharpShooter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            EloBuddy.SDK.Events.Loading.OnLoadingComplete += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Initializer.Initialize();
+        }
+    }
+}
---
+++ [xcsoft] SharpShooter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Sharpshooter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EloBuddy.SDK.Events.Loading.OnLoadingComplete += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            SharpShooter.Load();
+        }
+    }
+}
